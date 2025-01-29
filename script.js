8f95d9b9b22109c846a277e62a11d71073c11804
Rituraj Kumar Verma <riturajverma340@gmail.com>
2025-01-29 19:05:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function convert() {
+    const degrees = parseFloat(document.getElementById("degrees").value);
+    const type = document.getElementById("type").value;
+    let result = "";
+  
+    if (isNaN(degrees)) {
+      result = "Please enter a valid number";
+    } else {
+      if (type === "Fahrenheit") {
+        result = ((degrees - 32) * 5 / 9).toFixed(4) + " °C";
+      } else {
+        result = ((degrees * 9 / 5) + 32).toFixed(4) + " °F";
+      }
+    }
+  
+    document.getElementById("result").innerText = `Result: ${result}`;
+  }
+  
